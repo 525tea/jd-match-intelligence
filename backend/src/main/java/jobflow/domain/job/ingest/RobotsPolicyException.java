44ada41b4ac216f1c3c479f9fedb6b53ab25e46d
Lee Ye Jin <jene0028@gmail.com>
2026-06-04 08:47:34 +0900

    feat: validate crawler URLs with robots policy

---
+++ backend/src/main/java/jobflow/domain/job/ingest/RobotsPolicyException.java
@@ -0,0 +1,8 @@
+package jobflow.domain.job.ingest;
+
+public class RobotsPolicyException extends RuntimeException {
+
+    public RobotsPolicyException(String message) {
+        super(message);
+    }
+}
