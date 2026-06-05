63d507ad836f7dd16146b2d51a422edc26170cfd
Lee Ye Jin <jene0028@gmail.com>
2026-06-05 10:55:09 +0900

    feat: add elasticsearch search configuration

--- backend/src/main/java/jobflow/BackendApplication.java
+++ backend/src/main/java/jobflow/BackendApplication.java
@@ -1,15 +1,18 @@
 package jobflow;
 
+import jobflow.domain.job.search.JobSearchProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
+@EnableConfigurationProperties(JobSearchProperties.class)
 @SpringBootApplication
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
 
 }
---
+++ backend/src/main/java/jobflow/domain/job/search/JobSearchProperties.java
@@ -0,0 +1,10 @@
+package jobflow.domain.job.search;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "app.search.elasticsearch")
+public record JobSearchProperties(
+        String url,
+        String indexName
+) {
+}
