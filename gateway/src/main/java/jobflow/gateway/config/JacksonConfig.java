78eee7be28b12f4f19453ead538c4a210ad152e4
Lee Ye Jin <jene0028@gmail.com>
2026-06-26 23:45:51 +0900

    fix: serialize gateway security event timestamps

--- gateway/src/main/java/jobflow/gateway/config/JacksonConfig.java
+++ gateway/src/main/java/jobflow/gateway/config/JacksonConfig.java
@@ -2,6 +2,7 @@ package jobflow.gateway.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,7 +14,7 @@ public class JacksonConfig {
     @ConditionalOnMissingBean
     public ObjectMapper objectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.findAndRegisterModules();
+        objectMapper.registerModule(new JavaTimeModule());
         objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         return objectMapper;
     }
